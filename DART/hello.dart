fb8d2e11d5608f7f8b829852f10401260829e0c7
David Nanjila <nanjiladavid2@gmail.com>
2024-03-04 12:46:34 +0300

    A function that uses list data type

--- DART/hello.dart
+++ DART/hello.dart
@@ -1,3 +1,12 @@
 void main(){
     print("Hello Dart");
+    listFunction();
+}
+//funcion that uses list in Dart
+void listFunction(){
+  List<String> names = ["Ken", "James", "John"];
+  List<int> ages = [23, 45, 67];
+  print("Your name is ${names[0]} and you are ${ages[0]} years old");
+  print("Your name is ${names[1]} and you are ${ages[1]} years old");
+  print("Your name is ${names[2]} and you are ${ages[2]} years old");
 }
