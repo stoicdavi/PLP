c051a093f7e09fc02d39213d6ee6ae8f05422db6
David Nanjila <nanjiladavid2@gmail.com>
2024-03-20 21:23:56 +0300

    add os.remove to delete my_file.txt every time the code is run to avoid conflicts, & use open_file(my_file.txt, x) as file: creates a file

--- PYTHON_ASSIGNMENTS/week5_file_handling/file_handling_assignment.py
+++ PYTHON_ASSIGNMENTS/week5_file_handling/file_handling_assignment.py
@@ -1,1 +1,16 @@
 from contextlib import contextmanager
+import os
+os.remove('my_file.txt')
+@contextmanager
+def open_file(file_name, mode):
+    file = open(file_name, mode)
+    try:
+        yield file
+    finally:
+        file.close()
+
+with open_file('my_file.txt', 'x') as file:
+    pass
+
+    
+
