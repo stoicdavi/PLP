ba8358b22d2ded73b433c751a87670834d8abde7
David Nanjila <nanjiladavid2@gmail.com>
2024-04-11 15:14:09 +0300

    Feat: Create a play fuction that controls the game

--- DART/OOP/ADVANCED-OOP/tictactoe.dart
+++ DART/OOP/ADVANCED-OOP/tictactoe.dart
@@ -27,4 +27,29 @@ void DisplayBoard(List gameDashBoard){//printing the current state of the board
   // }
 }
 
-// void play(List gameDashBoard)
+void play(List gameDashBoard, String player){
+  print("Player $player select your position 1 - 9");
+  //get the position from the player
+  int position = int.parse(stdin.readLineSync()!);
+  //check if the position is valid
+  if (position != null && position >= 1 && position <= 9){
+    var row = (position / 3).ceil() - 1;//calculate the row
+    var col = (position % 3) - 1;//calculate the column
+
+    if(gameDashBoard[row][col] == ' '){
+      gameDashBoard[row][col] = player;
+      //TODO- Create a fucntion to check the winner
+      checkWinner(gameDashBoard, player);
+    }
+    else{
+      print("Position already taken please select another position");
+      play(gameDashBoard, player);
+    }
+  
+  }else{
+    print("Invalid position please select a position between 1 - 9");
+    play(gameDashBoard, player);
+  }
+
+}
+
