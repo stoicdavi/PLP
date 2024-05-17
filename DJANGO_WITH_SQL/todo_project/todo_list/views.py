def57b227dc4785ebf1dd2f6f42b015c79e07d8d
David Nanjila <nanjiladavid2@gmail.com>
2024-05-17 11:31:06 +0300

    fix: No display due to typos

--- DJANGO_WITH_SQL/todo_project/todo_list/views.py
+++ DJANGO_WITH_SQL/todo_project/todo_list/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, redirect
 from .models import Task
 
 def task_list(request):
-    task = Task.objects.all()
-    return render(request, 'todo_list/task_list.html', {'task':'task'})
+    tasks = Task.objects.all()
+    return render(request, 'todo_list/task_list.html', {'tasks':tasks})
 
 def add_task(request):
     if request.method == 'POST':
